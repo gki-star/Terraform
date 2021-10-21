457a13d6cc3a7c16787d00c79e7e0ec82ce51b97
gki_star <grkirzhner@hotmail.com>
2021-10-21 06:57:12 +0000

    Add an IP Address to html page

--- Lession2/WebServer.tf
+++ Lession2/WebServer.tf
@@ -9,7 +9,8 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
-echo "<h2>Webserver IP</h2><br>Build by Terraform!" > /var/www/html/index.html
+myIP = `curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>Webserver IP: $myIP</h2><br>Build by Terraform!" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
 EOF
