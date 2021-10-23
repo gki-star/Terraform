6b7430a906b2b69ba14c613da138491ae38d8ba0
gki-star <86729313+gki-star@users.noreply.github.com>
2021-10-23 18:47:37 +0200

    Update DynamicSecurityGroup.tf

--- Lession5/DynamicSecurityGroup.tf
+++ Lession5/DynamicSecurityGroup.tf
@@ -8,30 +8,30 @@
       for_each = ["80", "443", "8080"]
      
      content {
-      from_port 	= ingress.value
-      to_port		= ingress.value
-      protocol 		= "tcp"
+      from_port 	  = ingress.value
+      to_port		    = ingress.value
+      protocol 		  = "tcp"
       cidr_blocks 	= ["0.0.0.0/0"]
       }
     } 
 
     ingress {
-      from_port         = 22
-      to_port           = 22
-      protocol          = "tcp"
-      cidr_blocks       = ["10.10.0.0/16"]
+      from_port    = 22
+      to_port      = 22
+      protocol     = "tcp"
+      cidr_blocks  = ["10.10.0.0/16"]
     }
 
     egress {
-      from_port 	= 0
-      to_port 		= 0
-      protocol 		= "-1"
-      cidr_blocks       = ["0.0.0.0/0"]
+      from_port 	  = 0
+      to_port 	   	= 0
+      protocol   		= "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
     }
 
     tags          = {
-      Name        = "devops-eu-1-client2-dsg"
-      Owner       = "Gregory.Kirzhner@vodafone.com"
+      Name         = "devops-eu-1-client2-dsg"
+      Owner        = "Gregory.Kirzhner@vodafone.com"
     }
  }
 
